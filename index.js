5b8a8a0af57270f2c97a2faee2a7b4bbc33d29f0
Mike Ackison <mike.ackison@shopify.com>
2021-01-15 02:49:02 +0000

    add nextISSTimesForMyLocation

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 // This will require and run our main fetch function.
 
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+// const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
 
-  console.log('It worked! Returned IP:' , ip);
-});
-
-
-// fetchCoordsByIP('173.206.150.163', () => {
-//   console.log("error", error, "data", data)
-
-// })
+//   console.log('It worked! Returned IP:' , ip);
+// });
 
 
+// // fetchCoordsByIP('173.206.150.163', () => {
+// //   console.log("error", error, "data", data)
 
-const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+// // })
 
-fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
 
-  console.log('It worked! Returned flyover times:' , passTimes);
-});
+
+// const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+// fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned flyover times:' , passTimes);
+// });
 
 const { nextISSTimesForMyLocation } = require('./iss');
 
@@ -38,5 +38,15 @@ nextISSTimesForMyLocation((error, passTimes) => {
     return console.log("It didn't work!", error);
   }
   // success, print out the deets!
-  console.log(passTimes);
+  printPassTimes(passTimes);;
 });
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
